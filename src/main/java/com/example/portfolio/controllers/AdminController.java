f506f153631311a28404f9f74277c654870ee747
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2023-04-14 00:22:16 +0300

    Добавил в админку смену ролей пользователям

--- src/main/java/com/example/portfolio/controllers/AdminController.java
+++ src/main/java/com/example/portfolio/controllers/AdminController.java
@@ -1,5 +1,7 @@
 package com.example.portfolio.controllers;
 
+import com.example.portfolio.models.User;
+import com.example.portfolio.models.enums.Role;
 import com.example.portfolio.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -8,6 +10,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -23,6 +28,18 @@ public class AdminController {
     @PostMapping("/admin/user/block/{id}")
     public String userBlock(@PathVariable("id") Long id) {
         userService.blockUser(id);
+        return "redirect:/admin";
+    }
+    @GetMapping("/admin/user/edit/{user}")
+    public String userEdit(@PathVariable("user") User user, Model model) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        return "user-edit";
+    }
+
+    @PostMapping("/admin/user/edit")
+    public String userEdit(@RequestParam("userId") User user, @RequestParam Map<String, String> form) {
+        userService.changeUserRoles(user, form);
         return "redirect:/admin";
     }
 }
--- src/main/java/com/example/portfolio/services/UserService.java
+++ src/main/java/com/example/portfolio/services/UserService.java
@@ -8,7 +8,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -22,7 +26,7 @@ public class UserService {
         if (userRepository.findByEmail(email) != null) return false;
         user.setActive(true);
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        user.getRoles().add(Role.ADMIN);
+        user.getRoles().add(Role.USER);
         log.info("Saving new User with email: {}", email);
         userRepository.save(user);
         return true;
@@ -35,7 +39,24 @@ public class UserService {
     public void blockUser(Long id) {
         User user = userRepository.findById(id).orElse(null);
         if (user != null){
-            user.setActive(false);
+            if (user.isActive()){
+                user.setActive(false);
+            } else {
+                user.setActive(true);
+            }
+        }
+        userRepository.save(user);
+    }
+
+    public void changeUserRoles(User user, Map<String, String> form) {
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+        user.getRoles().clear();
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
         }
         userRepository.save(user);
     }
